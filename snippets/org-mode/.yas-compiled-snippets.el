d3c3af47aae049a58113aae8b500362dcc0fa851
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2019-03-22 14:24:17 +0100

    org time

---
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("time" "`(format-time-string \"%R\")`\n$0" "time" nil nil nil "/usr/local/google/home/goncharov/.emacs.d/private/snippets/org-mode/time" nil nil)))
+
+
+;;; Do not edit! File generated at Mon Feb 25 12:18:54 2019
